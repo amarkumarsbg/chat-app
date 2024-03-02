4e01d4d5c4f9233d0e200e76c844cf73fce75b65
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-03-02 19:31:18 +0530

    project setup completed DB is also connncted

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,43 @@
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
+
+    const userExists = await User.findOne({ username });
+
+    if (userExists) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save(); // Save the new user to the database
+
+    res.status(201).json({
+      _id: newUser.fullName,
+      fullName: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal server Error" });
+  }
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "../backend/routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world!!");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
