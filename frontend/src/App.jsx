40d8d27077450e01d8246df68fe2132f76f392d9
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-03-03 10:43:06 +0530

    sign up UI complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import Login from "./pages/login/Login";
+//import Login from "./pages/login/Login";
+import Signup from "./pages/signup/SignUp";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <Signup />
     </div>
   );
 }
